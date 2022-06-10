dbaa6b11f0c58bdd54b53bf58392f50ff0e55c4e
Mlinzo <vlad.derevyagin89@gmail.com>
2022-06-10 12:05:51 +0300

    connect alarmmap api

--- utils/other.utils.js
+++ utils/other.utils.js
@@ -1,5 +1,16 @@
 const { v4 } = require('uuid');
 
+const formatDate = (date) => {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+
+    return [year, month, day].join('-');
+}
+
 const arrDiff = (arr1, arr2) => {
     return arr1.filter((el1) => !arr2.includes(el1));
 }
@@ -39,6 +50,7 @@ const translateAlerts = (arr) => {
 const generateID = () => v4();
 
 module.exports = {
+    formatDate,
     arrDiff,
     generateID,
     isUndefined,
